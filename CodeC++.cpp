4d2850eef7254ec2129865a3c18da4c1b123c3f0
Phùng Tuấn Huy <huythelep@gmail.com>
2026-03-06 00:14:56 +0700

    Add project files.

---
+++ CodeC++.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+
+bool songuyento(int n)
+{
+	if (n < 2) return false;
+	for(int i = 2; i < sqrt(n); i++)
+	{
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+
+
+int main()
+{
+	int n;
+	cout << "Nhap vao n: ";
+	cin >> n;
+
+    if(songuyento(n))
+	{
+		cout << n << " la so nguyen to." << endl;
+	}
+	else
+	{
+		cout << n << " khong la so nguyen to." << endl;
+	}
+
+
+}
+
+
+//int main()
+//{
+//	int n, sum = 0;
+//	cout << "Nhap vao so luong so nguyen: ";
+//	cin >> n;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		sum += i;
+//	}
+//	cout << "Tong cac co nguyen tu 0 den" << n - 1 << " la: " << sum << endl;
+//	return 0;
+//}
+
+//
+//int n, a[100];
+//cout << "Nhap vao so nguyen duong n: ";
+//cin >> n;
+//
+//for (int i = 0; i < n; i++)
+//{
+//	cout << "Nhap vao so nguyen thu " << i + 1 << "";
+//	cin >> a[i];
+//}
+//int sum = 0;
+//for (int j = 0; j < n; j++)
+//{
+//	sum += a[j];
+//}
+//
+//cout << "Tong cac so nguyen da nhap la: " << sum << endl;
+//return 0;
